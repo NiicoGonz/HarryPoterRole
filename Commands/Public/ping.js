660b844f4c4bb4ad75e9157b73cd627eb60f21df
Niico <nicolas.gonz.mart@gmail.com>
2022-12-30 13:53:58 -0500

    modificando comando ping y usando Embed

---
+++ Commands/Public/ping.js
@@ -0,0 +1,25 @@
+const {SlashCommandBuilder, CommandInteraction, PermissionFlagsBits, EmbedBuilder} = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Pong")
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator), // only allowed for admin users
+  async  execute(interaction, client) {
+
+        const message = await interaction.deferReply({
+            fetchReply: true
+        });
+
+        const exampleEmbed = new EmbedBuilder()
+        .setColor(10038562)
+        .setAuthor({ name: `Lechuza de: ${interaction.member.user.username}` , iconURL: 'https://img.freepik.com/premium-vector/collection-vector-magic-fairy-tale-elements-icons-illustrations-book-glasses_71599-7229.jpg?w=2000' })
+        .setDescription(`
+        🏓 Pong!
+        📨 Latencia usuario: ${client.ws.ping}ms
+        🍡 Api latencia: ${message.createdTimestamp- interaction.createdTimestamp}ms`)
+        .setTimestamp()
+
+        await interaction.editReply({ content: ' ', ephemeral: true, embeds: [exampleEmbed ]}   )
+    },
+};
